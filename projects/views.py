5dcff0c20bd03d091c3817b326796fc53977c511
Алексей Розеган <rozeganalexey@gmail.com>
2026-01-12 12:37:32 +0100

    Create CRUD base functionals for projects app

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from .models import Project
+from .forms import ProjectForm
+
+
+@login_required
+def project_list(request):
+    projects = Project.objects.filter(owner=request.user)
+    return render(request, "projects/project_list.html", {"projects": projects})
+
+
+@login_required
+def project_create(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = request.user
+            project.save()
+            return redirect("projects:list")
+    else:
+        form = ProjectForm()
+    return render(request, "projects/project_form.html", {"form": form})
+
+
+@login_required
+def project_update(request, pk):
+    project = get_object_or_404(Project, pk=pk, owner=request.user)
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect("projects:list")
+    else:
+        form = ProjectForm(instance=project)
+    return render(request, "projects/project_form.html", {"form": form})
+
+
+@login_required
+def project_delete(request, pk):
+    project = get_object_or_404(Project, pk=pk, owner=request.user)
+    if request.method == "POST":
+        project.delete()
+        return redirect("projects:list")
+    return render(request, "projects/project_confirm_delete.html", {"project": project})
