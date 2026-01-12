ca27c1ad89765938c079f34a8cc0d72a5a712994
Алексей Розеган <rozeganalexey@gmail.com>
2026-01-12 12:03:50 +0100

    Start work on Task_Manager development. Started with projects folder. Created models for project/models.py

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="projects"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
