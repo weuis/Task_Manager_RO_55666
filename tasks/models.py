eb6a770e83cac93436b8ebefb534d3c5e96642e6
Алексей Розеган <rozeganalexey@gmail.com>
2026-01-13 16:55:28 +0100

    Start working on tasks app with models.py file

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,33 @@
+from django.conf import settings
 from django.db import models
+from projects.models import Project
 
-# Create your models here.
+
+class Task(models.Model):
+
+    class Status(models.TextChoices):
+        TODO = "TODO", "To Do"
+        IN_PROGRESS = "IN_PROGRESS", "In Progress"
+        DONE = "DONE", "Done"
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.TODO
+    )
+
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
