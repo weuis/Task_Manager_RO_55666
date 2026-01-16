abc5c2be55974b4003deb20660a3f453eaa8680a
Алексей Розеган <rozeganalexey@gmail.com>
2026-01-16 18:46:12 +0100

    Create routing for users app

---
+++ users/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = "users"
+
+urlpatterns = [
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
+    path("delete/", views.delete_account, name="delete"),
+
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="users/login.html"),
+        name="login"
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(),
+        name="logout"
+    ),
+]
