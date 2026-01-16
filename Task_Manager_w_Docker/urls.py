5df1a67d999270eea3d09934e7ed8ed554d34c33
Алексей Розеган <rozeganalexey@gmail.com>
2026-01-16 18:47:58 +0100

    Connect users app with main directory Task_Manager_w_Docker

--- Task_Manager_w_Docker/urls.py
+++ Task_Manager_w_Docker/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("users/", include("users.urls")),
     path("", include("tasks.urls")),
 ]
