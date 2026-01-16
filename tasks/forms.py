fafd2ebb3fcd616cebdd66472ec7ce4195d10d08
Алексей Розеган <rozeganalexey@gmail.com>
2026-01-16 19:01:01 +0100

    Update tasks/forms.py file after adding to tasks/models.py new model

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Task
+from .models import Task, TaskAssignment
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["title", "description", "status"]
+
+
+class TaskAssignmentForm(forms.ModelForm):
+    class Meta:
+        model = TaskAssignment
+        fields = ["user"]
