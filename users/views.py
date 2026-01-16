4996111fca5097b16b64e54f600a1229abb026ef
Алексей Розеган <rozeganalexey@gmail.com>
2026-01-16 18:44:57 +0100

    Create views.py file for users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from .forms import RegisterForm, UserUpdateForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("projects:list")
+    else:
+        form = RegisterForm()
+
+    return render(request, "users/register.html", {"form": form})
+
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("users:profile")
+    else:
+        form = UserUpdateForm(instance=request.user)
+
+    return render(request, "users/profile.html", {"form": form})
+
+
+def delete_account(request):
+    if request.method == "POST":
+        request.user.delete()
+        return redirect("login")
+
+    return render(request, "users/confirm_delete.html")
