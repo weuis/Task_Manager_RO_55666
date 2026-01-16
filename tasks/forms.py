7917de9bab7cbffa7caf589cb828d66c90c3d960
Алексей Розеган <rozeganalexey@gmail.com>
2026-01-16 18:35:02 +0100

    Rollback :)

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,17 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import Task
 
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
+class TaskForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ["username", "email", "password1", "password2"]
-
-
-class UserUpdateForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ["username", "email"]
+        model = Task
+        fields = ["title", "description", "status"]
