c217d3ed03fc3cf3e3edc2c3d71812fe05703ac6
Алексей Розеган <rozeganalexey@gmail.com>
2026-01-17 17:36:48 +0100

    Create models for comments section

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
+from tasks.models import Task
 
-# Create your models here.
+
+class Comment(models.Model):
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.task}"
+
